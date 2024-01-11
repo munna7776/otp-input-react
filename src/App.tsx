81ba796f867653e6e1ff01be23312aab72d11d74
Munna Kumar Barnwal <barnwalmunnakumar@gmail.com>
2024-01-11 16:56:30 +0000

    fix:updated otp navigation and fixed issues as well.

--- src/App.tsx
+++ src/App.tsx
@@ -1,75 +1,20 @@
-import { ChangeEvent, KeyboardEvent, useRef, useState } from "react";
+import { useState } from "react";
+import { OtpInput } from "./Input";
 
 const otpLength = 6;
 
 const App = () => {
-
-  const [otp, setOtp] = useState<string[]>(Array(otpLength).fill(""))
-  const [activeInputIndex,setActveInputIndex] = useState<number>(0)
-  const inputRefs = useRef<Array<HTMLInputElement | null>>([])
-
-
-  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
-    const { value } = e.target;
-    if(value.trim().length === 1) {
-      const newOtp = [...otp]
-      newOtp[activeInputIndex] = value
-      setOtp(newOtp)
-      changeFocusToNextOrPrevInput(activeInputIndex+1)
-    }
-  }
-
-  const changeFocusToNextOrPrevInput = (index: number) => {
-    if(index < otpLength) {
-      inputRefs.current[index]?.focus()
-      setActveInputIndex(index)
-    }
-  }
-
-  const handleKeyDownEvent = (e: KeyboardEvent<HTMLInputElement>) => {
-    const newOtp = [...otp]
-    if(e.key === "ArrowLeft") {
-      changeFocusToNextOrPrevInput(activeInputIndex-1)
-    } else if(e.key === "ArrowRight") {
-      changeFocusToNextOrPrevInput(activeInputIndex+1)
-    } else if(e.key === "Backspace") {
-      newOtp[activeInputIndex] = ''
-      setOtp(newOtp)
-      changeFocusToNextOrPrevInput(activeInputIndex-1)
-    } else if(e.key === "Delete") {
-      newOtp[activeInputIndex] = ''
-      setOtp(newOtp)
-      changeFocusToNextOrPrevInput(activeInputIndex-1)
-    } else {
-      return ;
-    }
-  }
-  console.log(otp.join(""))
+  const [otp, setOtp] = useState<string[]>(Array(otpLength).fill(""));
+  console.log(otp.join(""));
   return (
     <main className="grid place-items-center w-screen h-screen">
-      <div className="flex gap-2"  >
-        {
-          [...Array(6)].map((_,index) => {
-            return (
-              <input  
-                key={index} 
-                type="text"
-                maxLength={1} 
-                ref={(element) => inputRefs.current[index] = element}
-                data-testid={`input-${index}`}
-                value={otp[index]}
-                onChange={handleChange}
-                autoFocus={index===0}
-                onKeyDown={handleKeyDownEvent}
-                className="focus:outline-none border-2 border-[#C7D0D6] focus:border-[#A1A9B0] h-[44px] w-[44px] text-center rounded text-lg text-[#A1A9B0]" 
-              />
-            )
-          })
-        }
-      </div>
+      <OtpInput
+        otpLength={otpLength}
+        value={otp}
+        onChange={(value) => setOtp(value)}
+      />
     </main>
-  )
-}
+  );
+};
 
-export default App
-
+export default App;
--- src/Input.tsx
+++ src/Input.tsx
@@ -1,29 +1,84 @@
-import React, { useRef } from "react";
+import React, { KeyboardEvent, useRef, useState } from "react";
 
-type InputProps = {
-  index: number;
-  handleChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
-  activeInputIndex: number;
+type Props = {
+  otpLength: number;
+  value: string[];
+  onChange: (value: string[]) => void;
 };
 
-const Input = ({ handleChange, index, activeInputIndex }: InputProps) => {
-  const inputRef = useRef<HTMLInputElement>(null);
+export const OtpInput = ({ otpLength, value: otpValue, onChange }: Props) => {
+  const [activeInputIndex, setActiveInputIndex] = useState<number>(0);
+  const inputRefs = useRef<Array<HTMLInputElement | null>>([]);
 
+  const changeOtpValue = (code: string) => {
+    const currentInputRef = inputRefs.current[activeInputIndex];
+    if (!currentInputRef) {
+      return;
+    }
+    const newOtp = [...otpValue];
+    newOtp[activeInputIndex] = code;
+    currentInputRef.value = code;
+    onChange(newOtp);
+  };
+
+  const changeFocusToNextOrPrevInput = (index: number) => {
+    const newActiveIndex = Math.max(Math.min(otpLength - 1, index), 0);
+    if (inputRefs.current[newActiveIndex]) {
+      inputRefs.current[newActiveIndex]?.focus();
+      setActiveInputIndex(newActiveIndex);
+    }
+  };
+
+  const handleKeyDownEvent = (e: KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "ArrowLeft") {
+      e.preventDefault();
+      changeFocusToNextOrPrevInput(activeInputIndex - 1);
+    } else if (e.key === "ArrowRight") {
+      e.preventDefault();
+      changeFocusToNextOrPrevInput(activeInputIndex + 1);
+    } else if (e.key === "Backspace") {
+      e.preventDefault();
+      changeOtpValue("");
+      changeFocusToNextOrPrevInput(activeInputIndex - 1);
+    } else if (e.key === "Delete") {
+      e.preventDefault();
+      changeOtpValue("");
+    } else if (["Spacebar", "Space", "ArrowUp", "ArrowDown"].includes(e.key)) {
+      e.preventDefault();
+    } else if (new RegExp("^([0-9])$").test(e.key)) {
+      e.preventDefault();
+      changeOtpValue(e.key);
+      changeFocusToNextOrPrevInput(activeInputIndex + 1);
+    }
+  };
 
   return (
-    <input
-      type="number"
-      maxLength={1}
-      min={0}
-      max={9}
-      ref={inputRef}
-      data-testid={`input-${index}`}
-    //   disabled={index > activeInputIndex}
-      onChange={handleChange}
-      autoFocus={index === 0}
-      className="focus:outline-none border-2 border-[#C7D0D6] focus:border-[#A1A9B0] h-[44px] w-[44px] text-center rounded text-lg text-[#A1A9B0]"
-    />
+    <div
+      role="group"
+      aria-labelledby="one-time-password"
+      className="flex gap-[0.8rem]"
+    >
+      {[...Array(otpLength)].map((_, index) => {
+        return (
+          <input
+            key={index}
+            type="number"
+            inputMode="numeric"
+            dir="ltr"
+            maxLength={1}
+            ref={(element) => (inputRefs.current[index] = element)}
+            autoFocus={index === 0}
+            onFocus={() => setActiveInputIndex(index)}
+            onBlur={() =>
+              setActiveInputIndex((prev) => (prev === 0 ? 0 : prev - 1))
+            }
+            onKeyDown={handleKeyDownEvent}
+            data-value={otpValue[index]}
+            aria-label={`Please enter otp character ${index + 1}`}
+            className="border border-[#CDCDD659] focus:border-[#CDCDD6] w-[60px] h-[60px] text-center rounded-[10px] text-[1.7rem] font-almoni text-[#2A323F] bg-[#F5F6FA]"
+          />
+        );
+      })}
+    </div>
   );
 };
-
-export default Input;
